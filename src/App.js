39c9531a017ef85c599a40715ee2692bdda5a002
Manezeu Patricia <pmanezeu@gmail.com>
2022-07-08 08:48:44 +0100

    FIxed the linter errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Navbar from './components/Navbar';
 import Books from './components/Books';
 import Categories from './components/Categories';
