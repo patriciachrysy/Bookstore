5c682a4afb548b33490bfc91ead5709ec21763e8
Manezeu Patricia <pmanezeu@gmail.com>
2022-07-15 01:56:42 +0100

    Connected the Bookform component to the state

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,23 +1,13 @@
 import React from 'react';
-// import { v4 as uuidv4 } from 'uuid';
 import { useSelector } from 'react-redux';
 import Book from './Book';
-// import BookForm from './BookForm';
+import BookForm from './BookForm';
 
 const Books = () => {
   const books = useSelector((state) => state.books);
-  // const [books, setBooks] = useState([]);
 
   /* const deleteBook = (bookId) => {
     setBooks(books.filter((book) => book.id !== bookId));
-  };
-
-  const addBook = (bookTitle, bookAuthor) => {
-    setBooks([...books, {
-      id: uuidv4(),
-      title: bookTitle,
-      author: bookAuthor,
-    }]);
   }; */
 
   return (
@@ -27,7 +17,7 @@ const Books = () => {
             books.map((book) => <Book key={book.id} book={book} />)
             }
       </ul>
-      {/* <BookForm addBookProps={addBook} /> */}
+      <BookForm />
     </div>
   );
 };
