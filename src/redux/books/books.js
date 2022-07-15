733b87e4ee6d8307f608b3348d88b8d724579f89
Manezeu Patricia <pmanezeu@gmail.com>
2022-07-15 14:16:40 +0100

    Added action create to init app and retrieve books with actions. Update the reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,9 @@
+import createApp from "../../services/AppService";
+import BooksService from "../../services/BooksService";
+
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const RETRIVE_BOOKS = 'bookstore/books/RETRIVE_BOOKS';
 
 const initialState = [
   { id: 0, title: 'Moby Dick', author: 'Charles Dickens' },
@@ -7,9 +11,29 @@ const initialState = [
   { id: 2, title: 'Meurtre au presbytère', author: 'Agatha Christie' },
 ];
 
+const initApp = async () => {
+  try {
+    const res = await createApp();
+    localStorage.setItem('appId', res.data);
+    return Promise.resolve('App initialized successfully');
+  }catch(err){
+    return Promise.reject(`Failed to init the app: ${err}`);
+  }
+}
+
 export const addBook = (payload) => ({ type: ADD_BOOK, payload });
 
 export const removeBook = (payload) => ({ type: REMOVE_BOOK, payload });
+
+export const retrieveBooks = () => async(dispatch) => {
+  let appId = localStorage.getItem(appId);
+  try {
+    const res = await BooksService.getAll(appId);
+    dispatch({type: RETRIVE_BOOKS, payload: res.data});
+  }catch(err){
+    console.log(err);
+  }
+}
 
 const books = (state = initialState, action) => {
   switch (action.type) {
@@ -17,6 +41,8 @@ const books = (state = initialState, action) => {
       return [...state, action.payload];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload);
+    case RETRIVE_BOOKS:
+      return action.payload;
     default:
       return state;
   }
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import categories from './categories/categories';
 import books from './books/books';
 
@@ -7,6 +8,6 @@ const store = configureStore({
     books,
     categories,
   },
-});
+}, applyMiddleware(thunk));
 
 export default store;
