677abbf63574923484a539ed1198803a2a89f66b
Manezeu Patricia <pmanezeu@gmail.com>
2022-07-16 07:15:41 +0100

    Added elements to stick to the design

--- src/components/Book.js
+++ src/components/Book.js
@@ -14,10 +14,38 @@ const Book = (props) => {
 
   return (
     <li className="book">
-      <span className="book-category">{book.category}</span>
-      <h1>{book.title}</h1>
-      <span className="book-author">{book.author}</span>
-      <button type="button" onClick={() => handleClick()}>Remove</button>
+      <div className="book-infos">
+        <span className="book-category">{book.category}</span>
+        <h1>{book.title}</h1>
+        <span className="book-author">{book.author}</span>
+        <div className="buttons">
+          <button type="button">Comment</button>
+          <button type="button" onClick={() => handleClick()}>Remove</button>
+          <button type="button">Edit</button>
+        </div>
+      </div>
+      <div className="progress">
+        {/* <div className="progress-circle-wrapper">
+          <div className="progress-circle-inner" />
+        </div> */}
+        <div className="circle-wrap">
+          <div className="inside-circle" />
+          <div className="circle">
+            <div className="mask full">
+              <div className="fill" />
+            </div>
+          </div>
+        </div>
+        <div className="data">
+          <span>8%</span>
+          <span>Completed</span>
+        </div>
+      </div>
+      <div className="chapter-infos">
+        <span>Current Chapter</span>
+        <span>Chapter 3: &quot;A lesson learned&quot;</span>
+        <button type="button">Update progress</button>
+      </div>
     </li>
   );
 };
